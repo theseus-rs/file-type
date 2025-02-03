e13669e6f4b51a4a4af657a1cd7d236aa35e5586
brianheineman <brian.heineman@gmail.com>
2025-02-02 17:52:29 -0700

    test: improve test coverage

--- file_type/src/format/file_format.rs
+++ file_type/src/format/file_format.rs
@@ -105,9 +105,46 @@ mod tests {
     use super::*;
     use crate::format::{ByteSequence, PositionType};
 
+    #[test]
     fn test_format_id() {
         let source_type = SourceType::Custom;
         assert_eq!(source_type.format_id(1), "custom/1");
+    }
+
+    #[test]
+    fn test_priority() {
+        assert_eq!(SourceType::Custom.priority(), 1);
+        assert_eq!(SourceType::Default.priority(), 0);
+        assert_eq!(SourceType::Httpd.priority(), 5);
+        assert_eq!(SourceType::Linguist.priority(), 4);
+        assert_eq!(SourceType::Pronom.priority(), 2);
+        assert_eq!(SourceType::Wikidata.priority(), 3);
+    }
+
+    #[test]
+    fn test_partial_cmp() {
+        assert_eq!(
+            SourceType::Custom.partial_cmp(&SourceType::Custom),
+            Some(Ordering::Equal)
+        );
+        assert_eq!(
+            SourceType::Custom.partial_cmp(&SourceType::Default),
+            Some(Ordering::Greater)
+        );
+        assert_eq!(
+            SourceType::Default.partial_cmp(&SourceType::Custom),
+            Some(Ordering::Less)
+        );
+    }
+
+    #[test]
+    fn test_cmp() {
+        assert_eq!(SourceType::Custom.cmp(&SourceType::Custom), Ordering::Equal);
+        assert_eq!(
+            SourceType::Custom.cmp(&SourceType::Default),
+            Ordering::Greater
+        );
+        assert_eq!(SourceType::Default.cmp(&SourceType::Custom), Ordering::Less);
     }
 
     #[test]
